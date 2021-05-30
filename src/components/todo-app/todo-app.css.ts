aa16336747dab9a81ebc299a42e5a91d565e4951
Fabien LH <fabien.lehouedec@gmail.com>
2021-05-31 00:43:12 +0200

    Migrate to a single component <todo-app> with multiple rendering functions

--- src/components/todo-app/todo-app.css.ts
+++ src/components/todo-app/todo-app.css.ts
@@ -27,4 +27,85 @@ export default css`
   .column > * {
     margin-bottom: 10px;
   }
+
+  .todo-item {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 3px;
+  }
+
+  .add-todo {
+    border: none;
+    border-bottom: solid 1px #ccc;
+    background: none;
+    outline: none;
+    margin-top: 5px;
+    padding-bottom: 5px;
+    transition: 0.3s;
+  }
+  .add-todo::placeholder {
+    font-style: italic;
+  }
+  .add-todo:hover {
+    border-bottom: solid 1px #5bc0de;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .text {
+    transition: 0.3s;
+  }
+
+  .text.done {
+    text-decoration: line-through;
+    font-style: italic;
+    color: #666;
+  }
+
+  .checkbox, .delete-btn {
+    background: none;
+    border: none;
+    border-radius: 3px;
+    transition: 0.3s;
+    padding: 2px 6px;
+    margin-right: 5px;
+    cursor: pointer;
+  }
+
+  .delete-btn {
+    font-size: 12pt;
+  }
+
+  .checkbox:hover {
+    background-color: #eee;
+  }
+
+  .delete-btn:hover {
+    background-color: #c0392b;
+    color: white;
+  }
+
+  .checkbox.done {
+    background-color: #7dcea0;
+    color: white;
+  }
+
+  .checkbox.done:hover {
+    background-color: #229954;
+  }
+
+  .progress {
+    border-radius: 3px;
+    background-color: #eee;
+    height: 5px;
+  }
+  .todo-progress {
+    height: 5px;
+    border-radius: 3px;
+    background-color: #5bc0de;
+    transition: 0.3s;
+  }
 `
--- src/components/todo-app/todo-app.ts
+++ src/components/todo-app/todo-app.ts
@@ -1,14 +1,12 @@
 import { html, LitElement } from "lit";
 import { customElement, state } from "lit/decorators";
+import { classMap } from "lit/directives/class-map";
+import { styleMap } from "lit/directives/style-map";
 
 import TodoItemModel from "../../model/item";
 import styles from "./todo-app.css";
 
-// Required custom elements
-import "../todo-list/todo-list";
-import "../todo-add/todo-add";
-import "../todo-progress/todo-progress";
-import TodoService from "../../services/todos.service"
+import TodoService from "../../services/todos.service";
 
 @customElement("todo-app")
 export default class TodoAppElement extends LitElement {
@@ -18,35 +16,35 @@ export default class TodoAppElement extends LitElement {
   private _todos: TodoItemModel[] = TodoService.load();
 
   /**
-   * Handle 'toggle-done' event from <todo-list> element
-   * @param e the event fired by TodoListElement
+   * Toggle a todo done state
+   * @param index The item index in the list
    */
-  private _onToggleDone(e: CustomEvent) {
-    const index = e.detail;
+  private _onToggleDone(index: number) {
     this._todos = this._todos.map((todo, i) =>
       i === index ? { ...todo, done: !todo.done } : todo
     );
   }
 
   /**
-   * Handle 'delete-todo' event from <todo-list> element
-   * @param e the event fired by TodoListElement
+   * Delete a todo item from the list
+   * @param index The item index in the list
    */
-  private _onDeleteTodo(e: CustomEvent) {
-    const index = e.detail;
+  private _onDeleteTodo(index: number) {
     this._todos = this._todos.filter((_, i) => i !== index);
   }
 
   /**
-   * Handle 'todo-added' event from <todo-add> element
-   * @param e the event fired by TodoAddElement
+   * Add a todo in the list
+   * @param e The event
    */
-  private _onTodoAdded(e: CustomEvent) {
-    const text = e.detail;
+  private _onTodoAdded(e: KeyboardEvent) {
+    if (e.key !== "Enter") return;
+
+    const text = (e.target as HTMLInputElement).value;
+    if (!text?.trim?.()) return;
 
     // Filter predicates
-    if (this._todos.find((todo) => todo.text === text))
-      return;
+    if (this._todos.find((todo) => todo.text === text)) return;
 
     this._todos = [
       ...this._todos,
@@ -55,32 +53,87 @@ export default class TodoAppElement extends LitElement {
         done: false,
       },
     ];
+    (e.target as HTMLInputElement).value = "";
   }
 
   updated() {
     TodoService.save(this._todos);
   }
 
-  render() {
-    const done = this._todos.filter(todo => todo.done).length;
-    const total = this._todos.length;
-    const percent = done / total * 100;
+  renderAddTodo() {
+    return html`
+      <div>
+        <input type="checkbox" disabled />
+        <input
+          type="text"
+          placeholder="Add a new task"
+          class="add-todo"
+          @keyup=${this._onTodoAdded}
+        />
+      </div>
+    `;
+  }
 
+  renderTodoList() {
+    return html`
+      <div class="todo-list">
+        ${this._todos.map((todo, i) => this.renderTodo(todo, i))}
+      </div>
+    `;
+  }
+
+  renderTodo(todo: TodoItemModel, index: number) {
+    const classes = {
+      check: { checkbox: true, done: todo.done },
+      text: { text: true, done: todo.done },
+    };
+
+    return html`
+      <div class="todo-item">
+        <div
+          id="todo-check"
+          class=${classMap(classes.check)}
+          @click=${() => this._onToggleDone(index)}
+        >
+          ✓
+        </div>
+        <span id="todo-text" class=${classMap(classes.text)}>
+          ${todo.text}
+          <button
+            id="delete-btn"
+            class="delete-btn"
+            @click=${() => this._onDeleteTodo(index)}
+          >
+            ⨯
+          </button>
+        </span>
+      </div>
+    `;
+  }
+
+  renderProgress() {
+    const done = this._todos.filter((todo) => todo.done).length;
+    const total = this._todos.length;
+    const percent = (done / total) * 100;
+
+    const style = { width: `${percent}%` };
+    return html`
+      <div class="progress">
+        <div class="todo-progress" style=${styleMap(style)}>&nbsp;</div>
+      </div>
+      <span> <b>${done}</b> out of <b>${total}</b> tasks done </span>
+    `;
+  }
+
+  // Main rendering function
+  render() {
     return html`
       <div class="container">
         <h1>Todo List</h1>
         <div class="column">
-          <todo-list
-            .todos=${this._todos}
-            @toggle-done=${this._onToggleDone}
-            @delete-todo=${this._onDeleteTodo}
-          >
-          </todo-list>
-          <todo-add @todo-added=${this._onTodoAdded}></todo-add>
-          <todo-progress .percent=${percent}></todo-progress>
-          <span>
-            <b>${done}</b> out of <b>${total}</b> tasks done
-          </span>
+          <div>${this.renderTodoList()}</div>
+          <div>${this.renderAddTodo()}</div>
+          <div>${this.renderProgress()}</div>
         </div>
       </div>
     `;
